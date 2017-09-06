4d50737847be487faa8055999fcf98c97b56a901
AnnaAkibarova <Akibarova@gmail.com>
2017-09-06 22:59:10 +0300

    jj

--- js/script.js
+++ js/script.js
@@ -5,7 +5,29 @@ $("#arrcreate").on("click", function () {
     var elementsNum = +($("#num").val());
     var minNum = +($("#min").val());
     var maxNum = + ($("#max").val());
-
-    $(".placeForLog").append("<span>text</span>");
-   console.log("elNum :", elementsNum, "min :", minNum, "max ", maxNum);
+    var arr = createArr(elementsNum, minNum, maxNum);
+    for(var i = 0; i < arr.length; i++){
+        $(".placeForLog").append("<span>"+ arr[i] +"</span>");
+    }
 });
+$("#clear").on("click", function () {
+    $(".placeForLog span").empty();
+
+});
+$("#change").on("click", function () {
+    if($(".placeForLog").hasClass("inline")){
+        $(".placeForLog").removeClass("inline")
+    } else {
+        $(".placeForLog").addClass("inline")
+    }
+});
+
+
+
+function createArr(elementsNum, minNum, maxNum){
+    var arr = [];
+    for (var i = 0; i < elementsNum; i++){
+        arr.push(minNum + Math.random()*(maxNum - minNum));
+    }
+    return arr
+}
