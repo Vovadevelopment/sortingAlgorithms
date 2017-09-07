74e88cda3fce52fcca9cb62b3ffab0a1f834e5b8
AnnaAkibarova <Akibarova@gmail.com>
2017-09-08 00:45:16 +0300

    content

--- js/script.js
+++ js/script.js
@@ -1,33 +1,61 @@
-
-
-
-$("#arrcreate").on("click", function () {
-    var elementsNum = +($("#num").val());
-    var minNum = +($("#min").val());
-    var maxNum = + ($("#max").val());
-    var arr = createArr(elementsNum, minNum, maxNum);
-    for(var i = 0; i < arr.length; i++){
-        $(".placeForLog").append("<span>"+ arr[i] +"</span>");
+var arrayOfObj = [
+    {
+        name: "Vova",
+        weight: 90,
+        age: 26
+    },
+    {
+        name: "Kuzya",
+        weight: 7,
+        age: 5
+    },
+    {
+        name: "Anna",
+        weight: 49,
+        age: 21
+    },
+    {
+        name: "Chipa",
+        weight: 3,
+        age: 4
     }
-});
-$("#clear").on("click", function () {
-    $(".placeForLog span").empty();
+];
 
-});
-$("#change").on("click", function () {
-    if($(".placeForLog").hasClass("inline")){
-        $(".placeForLog").removeClass("inline")
-    } else {
-        $(".placeForLog").addClass("inline")
+function compileElements(array) {
+    for (var i = 0; i <array.length; i++){
+
+        $(".table-bordered tbody").append('<tr> <th scope="row">1</th> <td>'+ array[i].name +'</td> <td>'+ array[i].weight +'</td> <td>'+ array[i].age +'</td> </tr>');
+
+        console.log(array[i].name, array[i].weight, array[i].age);
     }
-});
 
-
-
-function createArr(elementsNum, minNum, maxNum){
-    var arr = [];
-    for (var i = 0; i < elementsNum; i++){
-        arr.push(minNum + Math.random()*(maxNum - minNum));
-    }
-    return arr
 }
+compileElements(arrayOfObj);
+
+$(".btn-generate").click (function () {
+    alert("hello")
+});
+$('.btn-age').click(function () {
+
+    arrayOfObj.sort(sortByKey);
+    $(".table-bordered tbody").html("");
+    compileElements(arrayOfObj);
+    
+});
+$('.btn-weight').click(function () {
+    alert("weight")
+
+});
+
+
+function sortByKey(key) {
+    function compareNumeric(a, b) {
+        return a[key] - b[key];
+    }
+    return compareNumeric;
+}
+
+
+
+
+console.log();
